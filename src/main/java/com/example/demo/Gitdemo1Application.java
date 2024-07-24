7b3682cdc24496f07cc2c2164512c4c4d1ae596c
huyihang <huyihang.2022@qq.com>
2024-07-24 09:30:58 +0800

    Git学习第一次提交

---
+++ src/main/java/com/example/demo/Gitdemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitdemo1Application.class, args);
+	}
+
+}
