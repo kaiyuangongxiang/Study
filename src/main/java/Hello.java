5efc3740f8b2d518d73f73e5a4df327785213612
huyihang <huyihang.2022@qq.com>
2024-07-24 09:35:09 +0800

    two

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: huyihang2022
+ * Date: 2024 -07 -24
+ * Time: 9:34
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
